369d4ef52055db634736686d816deb45cd2725cc
ialohcin <ialohcin.e2300@gmail.com>
2023-09-12 00:02:34 +0800

    Second

---
+++ app/src/main/java/com/example/nutrieye/NavigationScreen.java
@@ -0,0 +1,13 @@
+package com.example.nutrieye;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NavigationScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_screen);
+    }
+}
